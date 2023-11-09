dd8ca4acc0b98b01bac1b11b29f5b4f7454ebd4e
luisangelmaciel <89158021+luisangelmaciel@users.noreply.github.com>
2023-11-09 01:02:05 -0600

    slider

--- assets/boton-soporte-modal/script.js
+++ assets/boton-soporte-modal/script.js
@@ -1,6 +1,3 @@
-// INSERT JS HERE
-
-
 // SOCIAL PANEL JS
 const floating_btn = document.querySelector('.floating-btn');
 const close_btn = document.querySelector('.close-btn');
--- assets/fixing-the-overflow/README.md
+++
@@ -1,4 +0,0 @@
-# Fixing the Overflow
-
-A Pen created on CodePen.io. Original URL: [https://codepen.io/josetxu/pen/BaxLyyN](https://codepen.io/josetxu/pen/BaxLyyN).
-
